45cc734393705d5e64166f476aed479f66546ac4
michelint <lemoderateurdelive@gmail.com>
2024-09-23 11:39:32 +0200

    Maj : Ajout de la recherche du post a partir de l'id

--- test/list_post.php
+++ test/list_post.php
@@ -10,12 +10,32 @@ use App\Entity\Post;
  */
 $entityManager = require_once __DIR__ . "/../config/bootstrap.php";
 
-// Liste des postes
+
+echo PHP_EOL; // Pour de l'affichage
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////// Liste des postes //////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 echo "Liste des posts : \n";
 // Récupérer un PostRepository généré automatiquement par Doctrine
 $postRepository = $entityManager->getRepository(Post::class);
-$posts = $postRepository->findAll(); // Chaque element sera un objet de la classe post (le mapping se fera automatiquement)
+$posts = $postRepository->findAll(); // Chaque element sera un objet de la classe post (le mapping se fera automatiquement) SELECT * FROM posts
 foreach ($posts as $post) {
     echo "- ".$post->getTitre() . "\n";
 }
+echo PHP_EOL;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////// Lister un post recherché visa son ID //////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+echo "Liste du post ID=1 : \n";
+$post = $postRepository->find(1); // SELECT * FROM posts WHERE id_post=1
+if ($post) {
+    echo "- ".$post->getTitre()."\n \n";
+}else{
+    echo "Aucun post n'existe pour cet ID \n";
+}
+
 
