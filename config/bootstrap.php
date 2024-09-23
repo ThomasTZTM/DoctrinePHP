f91e8edb4273d5662249638e5d57b9425c0259b1
michelint <lemoderateurdelive@gmail.com>
2024-09-23 10:24:29 +0200

    Maj : Continuiter de la configuration de EntityManager dans le fichier /Config/Bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,6 +4,8 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Configuration de Autoload
+use Doctrine\DBAL\DriverManager;
+
 require_once __DIR__.'/../vendor/autoload.php';
 // Définir l'emplacement des entiter
 $path = [__DIR__.'/../src/Entity'];
@@ -11,3 +13,15 @@ $isDevMode = true;
 // Définition de la configuration des entités
 $configuration = \Doctrine\ORM\ORMSetup::createAttributeMetadataConfiguration($path, $isDevMode);
 // Définir les éléments de connexion à la BDD
+$configurationBD = [
+    'driver' => 'pdo_mysql',
+    'user' => 'root',
+    'password' => '',
+    'dbname' => 'db_posts',
+    'host' => 'localhost'
+];
+// Création de la connexion à la BDD
+$connexionBD = \Doctrine\DBAL\DriverManager::getConnection($configurationBD, $configuration);
+// Créer l'entityManager
+$entityManager = new \Doctrine\ORM\EntityManager($connexionBD, $configuration);
+return $entityManager;
