e8720046a50dd495dcee46a0ed8113ee49b3efd3
michelint <lemoderateurdelive@gmail.com>
2024-09-24 11:16:46 +0200

    MAJ : Création du fichier contexte_percistance pour les besoin du cours, on apprend ici les différent contexte de percistance

---
+++ test/contexte_percistance.php
@@ -0,0 +1,39 @@
+<?php
+
+// Récupération de l'EntityManager
+
+
+use App\Entity\Post;
+
+/**
+ * @var \Doctrine\ORM\EntityManager $entityManager
+ */
+$entityManager = require_once __DIR__ . "/../config/bootstrap.php";
+
+
+echo PHP_EOL; // Pour de l'affichage
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////// Récuperer des postes //////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$post = $entityManager
+    ->getRepository(Post::class)
+    ->find(9); // On choisi de victime l'id 9 ici
+
+$contextePercistance = $entityManager->getUnitOfWork();
+echo $contextePercistance->getEntityState($post)."\n"; // 1 MANAGED
+$entityManager->remove($post);
+echo $contextePercistance->getEntityState($post)."\n"; // 4 REMOVED
+
+// Créer un post
+$nvPost = new Post();
+$nvPost->setTitre("VDI - GMK est mort");
+$nvPost->setContenu("Je récupère toute ces voiture");
+$nvPost->setCreatedAt(new \DateTime());
+echo $contextePercistance->getEntityState($nvPost)."\n"; // 2 NEW
+
+$entityManager->persist($nvPost);
+echo $contextePercistance->getEntityState($nvPost)."\n"; // 1 MANAGED
+
+//$entityManager->flush(); j'ai pas envie d'exécuter
