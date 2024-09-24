a14bb1d432bce456718a11069cf17a9ebc2c416c
michelint <lemoderateurdelive@gmail.com>
2024-09-24 10:31:50 +0200

    Maj : Création du fichier qui sers à creer un post

---
+++ test/create_post.php
@@ -0,0 +1,30 @@
+<?php
+
+// Récupération de l'EntityManager
+
+
+use App\Entity\Post;
+
+/**
+ * @var \Doctrine\ORM\EntityManager $entityManager
+ */
+$entityManager = require_once __DIR__ . "/../config/bootstrap.php";
+
+
+echo PHP_EOL; // Pour de l'affichage
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////// Créer des postes //////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// On créer le post
+
+$post = new Post();
+$post->setTitre("VDI - Le mont evrest est facile à monter");
+$post->setContenu("Un nouveau contenu");
+$post->setCreatedAt(new \DateTime());
+
+// Demander à l'entityManager de persister l'entité $post dans la table posts
+
+$entityManager->persist($post); // n'existe pas directement le insert mais le prépare juste
+$entityManager->flush(); // On valide le insert
