e0c83b5eebc210ee7cbe47768c540b4b29447e2c
michelint <lemoderateurdelive@gmail.com>
2024-09-25 09:09:54 +0200

    Maj : nouveau script dans dql_post pour pouvoir obtenir les postes parues à partir d'une date donné

--- test/dql_post.php
+++ test/dql_post.php
@@ -17,6 +17,8 @@ echo PHP_EOL; // Pour de l'affichage
 /////////////////// Lister les post ou le nombre de like est superieur à X ////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+echo "\n Les posts qui ont plus de 10.000 likes sont : ";
+
 $nblikex = 10000;
 $dql = "SELECT p FROM App\Entity\Post p WHERE p.nbLikes >= :nblikex";
 // Création d'un objet "Requête" de la classe Query
@@ -26,5 +28,26 @@ $query->setParameter("nblikex", $nblikex);
 // Execution de la requete avec le mapping des enregistrement en objet Post
 $LesPosts=$query->getResult();
 foreach ($LesPosts as $post) {
-    echo "- ".$post->getTitre() . " | Le post comptablise ". $post->getNbLikes() . " likes\n";
+    echo "- ".$post->getTitre() . " | Le post à ". $post->getNbLikes() . " likes\n";
 }
+
+echo PHP_EOL;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////// Lister les post parue à partir d'une date donner //////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+echo "\n Les posts parue à partir de juillet 2024 : \n";
+
+$DateChoisie = DateTime::createFromFormat("d/m/Y", "01/07/2024" );
+$dql = "SELECT p FROM App\Entity\Post p WHERE p.createdAt > :DateChoisie ORDER BY p.createdAt ASC";
+// Création d'un objet "Requête" de la classe Query
+$query = $entityManager->createQuery($dql);
+// Donner une valeur au parametre ":nbLikex)
+$query->setParameter(":DateChoisie", $DateChoisie);
+// Execution de la requete avec le mapping des enregistrement en objet Post
+$LesPosts=$query->getResult();
+foreach ($LesPosts as $post) {
+    echo "- ".$post->getTitre(). "\n";
+}
+echo PHP_EOL;
